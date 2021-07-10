454daa722f9a0ef50e2b498f23ea2809a410a426
Your Name <you@example.com>
2021-07-10 20:17:45 +0600

    hw2

--- Tests/nhgng.py
+++ Tests/nhgng.py
@@ -1,2 +1,12 @@
-while True:
-    print("dawdadadadad")
+# student = {1: {"name": "Emma", "age":"27"},
+#            2: {"name": "Mike", "age":"28"}}
+#
+# # print(student[1]["age"])
+
+# def daf(*args):
+#     for i in args:
+#         print(i)
+#
+# daf(name='args')
+# a=[1,2,3,None,(),[] ]
+print(type(range(5)))
